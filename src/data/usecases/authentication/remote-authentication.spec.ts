ee4950edbbda2499ca30e97c4c80077612fc0298
Agmar Torres <agmartorres@gmail.com>
2021-06-26 12:52:51 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -80,7 +80,7 @@ describe('RemoteAuthentication', () => {
     await expect(promise).rejects.toThrow(new UnexpectorError())
   })
 
-  it('should ruten an AccountModel if HttpPostClient returns 200', async () => {
+  it('should return an AccountModel if HttpPostClient returns 200', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     const httpResult = mockAccountModel()
     httpPostClientSpy.response = {
