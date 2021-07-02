a7cd5741cb96acdd6c6547ca67e438be6c820a63
Agmar Torres <agmartorres@gmail.com>
2021-07-02 15:06:47 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/presetation/validation/required-field/required-field-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from '@/presetation/validation/protocols/field-validation'
+import { RequiredFieldError } from './../errors'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    return new RequiredFieldError()
+  }
+}
