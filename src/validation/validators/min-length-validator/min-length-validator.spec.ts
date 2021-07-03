f01117eb3b28d58389bd1fd9ca17ac65d1a5b625
Agmar Torres <agmartorres@gmail.com>
2021-07-03 11:29:56 -0300

    test: ensure that value is valid return error equals falsy

--- src/validation/validators/min-length-validator/min-length-validator.spec.ts
+++ src/validation/validators/min-length-validator/min-length-validator.spec.ts
@@ -2,9 +2,15 @@ import { InvalidFieldError } from '@/validation/errors'
 import { MinLengthValidation } from './min-length-validator'
 
 describe('MinLengthValidation', () => {
-  test('Shoul return error if value is invalid ', () => {
+  test('Should return error if value is invalid ', () => {
     const sut = new MinLengthValidation('password', 7)
     const error = sut.validate('123798')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value is valid ', () => {
+    const sut = new MinLengthValidation('password', 7)
+    const error = sut.validate('12379800')
+    expect(error).toBeFalsy()
   })
 })
