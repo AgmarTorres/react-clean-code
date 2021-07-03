78b1d6e1d5e6182701ef9cf4cce5b4c6e7dcaaf3
Agmar Torres <agmartorres@gmail.com>
2021-07-03 13:32:43 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,16 @@
+import { FieldValidationSpy } from '../test/mock-validation-composite'
+import { ValidationComposiste } from './validation-composite'
+
+describe('Validation Composite', () => {
+  test('should return error if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('any_error_message')
+    const sut = new ValidationComposiste([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validade('any_field', 'any_value')
+    expect(error).toBe('any_error_message')
+  })
+})
