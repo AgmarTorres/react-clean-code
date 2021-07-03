821d388f643d79d4b6b5bec6971ce9e443488845
Agmar Torres <agmartorres@gmail.com>
2021-07-03 13:35:32 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,6 +4,7 @@ import { ValidationComposiste } from './validation-composite'
 describe('Validation Composite', () => {
   test('should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
     fieldValidationSpy.error = new Error('any_error_message')
     const sut = new ValidationComposiste([
