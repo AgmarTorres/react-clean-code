0dbd9f3e92727826202fe5b7880b9220808fa7c1
Agmar Torres <agmartorres@gmail.com>
2021-07-03 10:15:01 -0300

    test: ensure EmailValidation if email is valid

--- src/validation/validators/email-validation/email-validation.spec.ts
+++ src/validation/validators/email-validation/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('Email Vaslidation', () => {
   test('should  return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('email')
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.random.word())
     expect(error).toStrictEqual(new InvalidFieldError())
+  })
+
+  test('should  return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.words())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
