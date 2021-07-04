48f3f97158174ee20a75f53025ada985f59e2fd1
Agmar Torres <agmartorres@gmail.com>
2021-07-04 13:28:50 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email-validation/email-validation'
+export * from './min-length-validator/min-length-validator'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
