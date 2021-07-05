96de7e5827948e163897f7ff6b7e27f760300b89
Agmar Torres <agmartorres@gmail.com>
2021-07-05 11:53:07 -0300

    refactor: move SUT creation to a factory method

--- src/presetation/components/input/input.spec.tsx
+++ src/presetation/components/input/input.spec.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presetation/context/form/form-context'
 
+const makeSut = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name="field" />
+    </Context.Provider>
+  )
+}
 describe('Input Component', () => {
   test('Should begin with readOnly', () => {
-    const { getByTestId } = render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name="field" />
-      </Context.Provider>
-    )
-    const input = getByTestId('field') as HTMLInputElement
+    const sut = makeSut()
+    const input = sut.getByTestId('field') as HTMLInputElement
     fireEvent.focus(input)
     expect(input.readOnly).toBe(false)
   })
