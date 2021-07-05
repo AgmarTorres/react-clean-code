1b64a07a9a25f9e758a2523f48de84b2469b8c3d
Agmar Torres <agmartorres@gmail.com>
2021-07-05 09:36:24 -0300

    feat: add Login Composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,5 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presetation/components'
 import '@/presetation/styles/global'
+import { makeLogin } from './factories/pages/login/login-factory'
 
-ReactDOM.render(<Router />, document.getElementById('main'))
+ReactDOM.render(
+  <Router makeLogin={makeLogin} />,
+  document.getElementById('main'))
--- src/validation/validators/min-length-validator/min-length-validator.ts
+++ src/validation/validators/min-length-validator/min-length-validator.ts
@@ -4,6 +4,6 @@ import { InvalidFieldError } from '@/validation/errors'
 export class MinLengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly minLength: number) {}
   validate (value: string): Error {
-    return !value || value.length > 6 ? null : new InvalidFieldError()
+    return !value || value.length > 4 ? null : new InvalidFieldError()
   }
 }
